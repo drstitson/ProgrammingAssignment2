3677f2e0bf5820359c095a2e8f7dd741d1d75964
drstitson <drstitson@doctors.org.uk>
2016-04-03 08:29:24 +0100

    my functions
    my attempt at the matrix inverse functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to set up cache matrix and return the inverse
 
-## Write a short comment describing this function
+## Set up the cache matrix as per the given example.
+## ie "setting" and "getting" the original matrix and the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+                set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setInv <- function(inverse) inv <<- inverse
+        getInv <- function() inv
+        list(set = set,
+             get = get,
+             setInv = setInv,
+             getInv = getInv)
 }
 
+## Return inverse matrix of input matrix ‘x’
 
-## Write a short comment describing this function
+## checks if inverse matrix is empty (does not contain null). 
+## if so returns the matrix, otherwise solves the inverse, 
+## using a temporary newMat.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInv()
+        if (!is.null(inv)) {
+                return(inv)
+        }
+        newMat <- x$get()
+        inv <- solve(newMat, ...)
+        x$setInv(inv)
+        inv
 }
